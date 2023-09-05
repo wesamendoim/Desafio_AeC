bd84289dbab7b968fc4cb2d1a3299fd3e8b1e665
wesamendoim <wesleyvbnardino@hotmail.com>
2023-09-05 15:27:02 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio_AeC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
